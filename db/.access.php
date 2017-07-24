5d96e69c6e2f92a018f13327333c29e75b9e2beb
André Camacho <info@humbledev.ch>
2017-07-24 12:04:57 +0100

    create db/access.php file

---
+++ db/.access.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of the Social Comparison Grade Report plugin for Moodle by André Camacho http://www.camacho.pt
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Capability definition for social comparison grade report
+ *
+ * @package   gradereport_scgr
+ * @copyright 2017 onwards André Camacho http://www.camacho.pt
+ * @author    André Camacho
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+    'gradereport/scgr:view' => array(
+        'riskbitmask' => RISK_PERSONAL,
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        )
+    )
+);
+
+
