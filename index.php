98ee0480045acfa1c0457d7b315f154ffe2fd1b6
André Camacho <info@humbledev.ch>
2017-08-26 13:17:15 +0200

    more tests

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ $PAGE->set_url(new moodle_url('/grade/report/scgr/index.php', array('id'=>$cours
 	
 	echo html_writer::tag('h3', 'hello darling');
 	
-	echo $OUTPUT->notification('waaaaaaaarning updated', 'notifymessage');
+	echo $OUTPUT->notification('waaaaaaaarning test updated', 'notifymessage');
 	echo $OUTPUT->notification('success', 'notifymessage');
 	
 	echo $OUTPUT->footer();
