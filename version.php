f516d69c9283a369b75b91f05e1953169491ec36
André Camacho <andre@camacho.pt>
2017-12-19 17:52:59 +0100

    version updated

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
  
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017112104;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version = 2017121901;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires = 2016120500;        // Requires this Moodle version
 $plugin->component = 'gradereport_scgr'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '0.1.1';
+$plugin->release   = '0.2.1';
