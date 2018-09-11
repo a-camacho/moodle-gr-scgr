b66b5acba0230c7fc2184fa9b923879f0902e4d6
André Camacho <andre@camacho.pt>
2018-09-11 10:13:47 +0200

    fixed localizated string

--- views/teacher.php
+++ views/teacher.php
@@ -22,133 +22,136 @@ if ( in_array($courseid, $courses_with_groups) ) {
     $course_has_groups = false;
 }
 
-// Include the form
-require_once($CFG->dirroot.'/grade/report/scgr/forms/choose_activities_form.php');
+if ( $course_has_groups == false ) {
+    echo html_writer::tag('p', get_string('nav_invalid_mode', 'gradereport_scgr') );
+} else {
+    // Include the form
+    require_once($CFG->dirroot.'/grade/report/scgr/forms/choose_activities_form.php');
+    if ( $view == 'progression' || $view == 'default' ) {
 
-if ( $view == 'progression' || $view == 'default' ) {
+        echo html_writer::tag('p', get_string('teacher_progression_description', 'gradereport_scgr') );
 
-    echo html_writer::tag('p', get_string('teacher_progression_description', 'gradereport_scgr') );
+        $activities = getActivitiesFromCourseID($courseid, $categoryid, false);
 
-    $activities = getActivitiesFromCourseID($courseid, $categoryid, false);
+        $forms_action_url = $CFG->wwwroot . '/grade/report/scgr/index.php?id=' . $courseid . '&section=teacher&view=progression';
+        $mform = new chooseactivities_form( $forms_action_url, array( $activities ) );
 
-    $forms_action_url = $CFG->wwwroot . '/grade/report/scgr/index.php?id=' . $courseid . '&section=teacher&view=progression';
-    $mform = new chooseactivities_form( $forms_action_url, array( $activities ) );
+        if ($mform->is_cancelled()) {
 
-    if ($mform->is_cancelled()) {
+        } else if ($fromform = $mform->get_data()) {
 
-    } else if ($fromform = $mform->get_data()) {
+            //Set default data and display form
 
-        //Set default data and display form
+            $toform = '';
+            $mform->set_data($toform);
+            $mform->display();
 
-        $toform = '';
-        $mform->set_data($toform);
-        $mform->display();
+            $data = $mform->get_data();
 
-        $data = $mform->get_data();
-
-        // Set group_id variable
-        if ( property_exists($data, "activity") ) {
-            $activities = $data->activity;
-        } else {
-            $activities = NULL;
-        }
-
-        foreach ( $user_groups as $group ) {
-
-            $users = getUsersFromGroup($group);
-            // Strip tutors from users
-            $users = stripTutorsFromUsers($users, $context);
-
-            // Create chart
-            $chart = new \core\chart_line();
-            $chart->set_smooth(true);
-
-            $color_array = array(   'd6d6d6', '#92ff80', '#ecff80', '#80ffc2', '#ffcb80', '#80cbff', '#8086ff',
-                                    '#a480ff', '#fb80ff', '#ff80bf', '#ff8080' );
-
-            $CFG->chart_colorset = $color_array;
-
-            foreach ($users as $user) {
-                $user_object = $DB->get_record('user', array('id'=>$user));
-                $username = $user_object->firstname . $user_object->lastname;
-                $series = new core\chart_series($username, getActivitiesGradeFromUserID($user, $courseid, $activities, true));
-                $series->set_type(\core\chart_series::TYPE_LINE);
-                $chart->add_series($series);
+            // Set group_id variable
+            if ( property_exists($data, "activity") ) {
+                $activities = $data->activity;
+            } else {
+                $activities = NULL;
             }
 
-            $chart->set_labels(getActivitiesNames($activities, $courseid));
-            echo $OUTPUT->render($chart);
-        }
+            foreach ( $user_groups as $group ) {
 
-    } else {
-        //Set default data (if any)
-        $toform = '';
-        $mform->set_data($toform);
-        //displays the form
-        $mform->display();
-    }
+                $users = getUsersFromGroup($group);
+                // Strip tutors from users
+                $users = stripTutorsFromUsers($users, $context);
 
-} elseif( $view == 'comparison' ) {
+                // Create chart
+                $chart = new \core\chart_line();
+                $chart->set_smooth(true);
 
-    echo html_writer::tag('p', get_string('teacher_comparison_description', 'gradereport_scgr') );
+                $color_array = array(   'd6d6d6', '#92ff80', '#ecff80', '#80ffc2', '#ffcb80', '#80cbff', '#8086ff',
+                    '#a480ff', '#fb80ff', '#ff80bf', '#ff8080' );
 
-    $activities = getActivitiesFromCourseID($courseid, $categoryid);
+                $CFG->chart_colorset = $color_array;
 
-    $forms_action_url = $CFG->wwwroot . '/grade/report/scgr/index.php?id=' . $courseid . '&view=comparison';
-    $mform = new chooseactivities_form( $forms_action_url, array( $activities ) );
+                foreach ($users as $user) {
+                    $user_object = $DB->get_record('user', array('id'=>$user));
+                    $username = $user_object->firstname . $user_object->lastname;
+                    $series = new core\chart_series($username, getActivitiesGradeFromUserID($user, $courseid, $activities, true));
+                    $series->set_type(\core\chart_series::TYPE_LINE);
+                    $chart->add_series($series);
+                }
 
-    if ($mform->is_cancelled()) {
-
-    } else if ($fromform = $mform->get_data()) {
-
-        //Set default data and display form
-        $toform = '';
-        $mform->set_data($toform);
-        $mform->display();
-        $data = $mform->get_data();
-
-        // Set group_id variable
-        if ( property_exists($data, "activity") ) {
-            $activities = $data->activity;
-        } else {
-            $activities = NULL;
-        }
-
-        foreach ( $user_groups as $group ) {
-
-            $users = getUsersFromGroup($group);
-            // Strip tutors from users
-            $users = stripTutorsFromUsers($users, $context);
-            $usernames = getUsernamesFromUsers($users);
-
-            // Create chart
-            $chart = new core\chart_bar();
-
-            $CFG->chart_colorset = ['#001f3f', '#d2d2d2', '#c2c2c2', '#b2b2b2', '#a2a2a2', '#929292', '#828282', '#727272'];
-
-            foreach ( $activities as $activity ) {
-                $series = new core\chart_series(getActivityName($activity, $courseid), getActivityGradeFromUsers($users, $courseid, $activity, true));
-                $chart->add_series($series);
+                $chart->set_labels(getActivitiesNames($activities, $courseid));
+                echo $OUTPUT->render($chart);
             }
 
-            $chart->set_labels($usernames);
+        } else {
+            //Set default data (if any)
+            $toform = '';
+            $mform->set_data($toform);
+            //displays the form
+            $mform->display();
+        }
 
-            echo $OUTPUT->render($chart);
+    } elseif( $view == 'comparison' ) {
+
+        echo html_writer::tag('p', get_string('teacher_comparison_description', 'gradereport_scgr') );
+
+        $activities = getActivitiesFromCourseID($courseid, $categoryid);
+
+        $forms_action_url = $CFG->wwwroot . '/grade/report/scgr/index.php?id=' . $courseid . '&view=comparison';
+        $mform = new chooseactivities_form( $forms_action_url, array( $activities ) );
+
+        if ($mform->is_cancelled()) {
+
+        } else if ($fromform = $mform->get_data()) {
+
+            //Set default data and display form
+            $toform = '';
+            $mform->set_data($toform);
+            $mform->display();
+            $data = $mform->get_data();
+
+            // Set group_id variable
+            if ( property_exists($data, "activity") ) {
+                $activities = $data->activity;
+            } else {
+                $activities = NULL;
+            }
+
+            foreach ( $user_groups as $group ) {
+
+                $users = getUsersFromGroup($group);
+                // Strip tutors from users
+                $users = stripTutorsFromUsers($users, $context);
+                $usernames = getUsernamesFromUsers($users);
+
+                // Create chart
+                $chart = new core\chart_bar();
+
+                $CFG->chart_colorset = ['#001f3f', '#d2d2d2', '#c2c2c2', '#b2b2b2', '#a2a2a2', '#929292', '#828282', '#727272'];
+
+                foreach ( $activities as $activity ) {
+                    $series = new core\chart_series(getActivityName($activity, $courseid), getActivityGradeFromUsers($users, $courseid, $activity, true));
+                    $chart->add_series($series);
+                }
+
+                $chart->set_labels($usernames);
+
+                echo $OUTPUT->render($chart);
+
+            }
+
+        } else {
+
+            //Set default data (if any)
+            $toform = '';
+            $mform->set_data($toform);
+            //displays the form
+            $mform->display();
 
         }
 
-    } else {
+    } elseif( $view == 'custom' ) {
 
-        //Set default data (if any)
-        $toform = '';
-        $mform->set_data($toform);
-        //displays the form
-        $mform->display();
+        include_once('modules/custom_graph.php');
 
     }
-
-} elseif( $view == 'custom' ) {
-
-    include_once('modules/custom_graph.php');
-
 }
