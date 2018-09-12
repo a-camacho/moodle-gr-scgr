ed2825e61cb7e9efd0d084fc359da3276eaf3163
André Camacho <andre@camacho.pt>
2018-09-12 12:13:00 +0200

    corrected navigation tabs : custom was not showing

--- lib.php
+++ lib.php
@@ -51,7 +51,7 @@ function printMainNavigation( $courseid, $course_has_groups, $studentview, $teac
             $listitem.='" href="index.php?id=' . $courseid . '&section=custom" title="' . get_string('nav_section_custom', 'gradereport_scgr') . '">';
             $listitem.=get_string('nav_section_custom', 'gradereport_scgr');
             $listitem.='</a></li>';
-            $listitem='<li class="nav-item">';
+            $listitem.='<li class="nav-item">';
             $listitem.='<a class="nav-link';
             $listitem.=($section == 'help') ? ' active' : '';
             $listitem.='" href="index.php?id=' . $courseid . '&section=help" title="' . get_string('nav_section_help', 'gradereport_scgr') . '">';
@@ -758,6 +758,7 @@ function getGrades($users, $courseid, $activity, $inpercentage = false) {
 
     foreach ($users as $user) {
         if ( !empty($grading_info->items) ) {
+
             $grade = $grading_info->items[0]->grades[$user]->grade;
 
             if ($inpercentage == true) {
