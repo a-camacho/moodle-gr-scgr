9a911e0ced6f44cad9b66fb411ec17a76e5c5707
André Camacho <andre@camacho.pt>
2018-09-13 14:02:39 +0200

    Revert "Fixes #8 -> Division by/with zero when  equals 0"
    This reverts commit 94a1c301fa264730378917e04d36755e70249bcc.

--- lib.php
+++ lib.php
@@ -718,12 +718,8 @@ function getGrades($users, $courseid, $activity, $inpercentage = false) {
             $grade = $grading_info->items[0]->grades[$user]->grade;
 
             if ($inpercentage == true) {
-                if ( $grade > 0 ) {
-                    $grade = $grade / $max_grade * 100;
-                    $grade = round($grade, 2);
-                } else {
-                    $grade = 0;
-                }
+                $grade = $grade / $max_grade * 100;
+                $grade = round($grade, 2);
             }
 
             array_push($grades, floatval($grade));
